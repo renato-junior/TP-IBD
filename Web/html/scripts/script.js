4cc529f6460c4ac96a4c54e84d487ad4335cdd46
EvandroLucas <lucasfigteixeira@gmail.com>
2018-11-25 22:28:01 -0200

    Update Web

--- Web/html/scripts/script.js
+++ Web/html/scripts/script.js
@@ -1,6 +1,32 @@
-import { hello } from './module.js';
-...
-// alert(hello());
-export function hello() {
-    return "Hello World";
-}
+
+
+    function nextPage(){
+        window.location.href="next.jsp"
+    }
+
+    var sqlControl = 0;
+    function sqlTest(){
+        if(sqlControl == 0){
+            document.getElementById("testTarget").innerHTML = "Oi";
+            sqlControl = 1;
+        }
+        else{
+            document.getElementById("testTarget").innerHTML = "Ei";
+            sqlControl = 0;
+        }
+        console.log("Apertou");
+    }
+
+    function testw3() {
+            
+        xmlhttp=new XMLHttpRequest();
+
+        xmlhttp.onreadystatechange=function() {
+            if (this.readyState==4 && this.status==200) {
+                document.getElementById("testTarget").innerHTML=this.responseText;
+            }
+        }
+        xmlhttp.open("test","Search.php",true);
+        xmlhttp.send();
+    }
+
