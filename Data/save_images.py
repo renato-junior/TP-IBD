b43e62c4fe5c3b3794437a89936947ac23f16975
Renato Júnior <renatosergiolopez@hotmail.com>
2018-11-28 19:43:44 -0200

    Add script to download images and insert then into db

---
+++ Data/save_images.py
@@ -0,0 +1,39 @@
+import sys
+import base64
+import MySQLdb
+import urllib.request
+ 
+db = MySQLdb.connect(host="192.168.0.200",  # your host 
+                     user="USER",       # username
+                     passwd="PASSWORD",     # password
+                     db="GameMultiple")   # name of the database
+
+cur = db.cursor()
+
+cur.execute("SELECT ID, HeaderImage FROM Game")
+
+result = cur.fetchall()
+
+rowcount = cur.rowcount
+
+i = 1
+
+for row in result :
+    print(i, "/", rowcount)
+    i+=1
+    try :
+        filename = urllib.request.urlretrieve(row[1])[0]
+        blob_value = open(filename, 'rb').read()
+        # encoded_string = base64.b64encode(blob_value)
+        sql = "INSERT INTO Image VALUES (%s,%s)"
+        args = (row[0], blob_value)
+        cur = db.cursor()
+        cur.execute(sql, args)
+        db.ping(True)
+    except (MySQLdb.Error, MySQLdb.Warning) as e:
+        print(e)
+    except:
+        print("URL inválida: ", row[1])
+
+db.commit()
+db.close()
