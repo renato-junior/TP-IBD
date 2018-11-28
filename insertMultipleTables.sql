43076c9bd95730af72cc308277ed97b11fc736da
Renato Júnior <renatosergiolopez@hotmail.com>
2018-11-28 10:40:31 -0200

    Fix game.csv import in insertMultipleTables.sql

--- insertMultipleTables.sql
+++ insertMultipleTables.sql
@@ -11,49 +11,50 @@ TRUNCATE TABLE Owner;
 TRUNCATE TABLE User;
 TRUNCATE TABLE Game;
 
-LOAD DATA INFILE 'game.csv'
+LOAD DATA LOCAL INFILE 'gamenew.csv'
 INTO TABLE Game
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
-IGNORE 1 LINES (ID,Name,ReleaseDate,RequiredAge,DemoCount,DeveloperCount,DLCCount,Metacritic,MovieCount,PackageCount,RecommendationCount,PublisherCount,ScreenshotCount,SteamSpyOwners,SteamSpyOwnersVariance,SteamSpyPlayersEstimate,SteamSpyPlayersVariance,AchievementCount,AchievementHighlightedCount,ControllerSupport,IsFree,FreeVerAvail,PurchaseAvail,SubscriptionAvail,PriceCurrency,PriceInitial,PriceFinal,SupportEmail,SupportURL,AboutText,Background,ShortDescrip,DetailedDescrip,DRMNotice,ExtUserAcctNotice,HeaderImage,LegalNotice,Reviews,SupportedLanguages,Website);
+IGNORE 1 LINES
+(ID,Name,ReleaseDate,RequiredAge,DemoCount,DeveloperCount,DLCCount,Metacritic,MovieCount,PackageCount,RecommendationCount,PublisherCount,ScreenshotCount,SteamSpyOwners,SteamSpyOwnersVariance,SteamSpyPlayersEstimate,SteamSpyPlayersVariance,AchievementCount,AchievementHighlightedCount,ControllerSupport,IsFree,FreeVerAvail,PurchaseAvail,SubscriptionAvail,@dummy,@dummy,@dummy,@dummy,@dummy,@dummy,PriceCurrency,PriceInitial,PriceFinal,SupportEmail,SupportURL,AboutText,Background,ShortDescrip,DetailedDescrip,DRMNotice,ExtUserAcctNotice,HeaderImage,LegalNotice,Reviews,SupportedLanguages,Website);
 
-LOAD DATA INFILE 'names.csv'
+LOAD DATA LOCAL INFILE 'names.csv'
 INTO TABLE User
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES (ID, Name);
 
-LOAD DATA INFILE 'owners.csv'
+LOAD DATA LOCAL INFILE 'owners.csv'
 INTO TABLE Owner
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES (UserID, GameID);
 
-LOAD DATA INFILE 'category.csv'
+LOAD DATA LOCAL INFILE 'category.csv'
 INTO TABLE Category
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES (ID,CategorySinglePlayer,CategoryMultiplayer,CategoryCoop,CategoryMMO,CategoryInAppPurchase,CategoryIncludeSrcSDK,CategoryIncludeLevelEditor,CategoryVRSupport);
 
-LOAD DATA INFILE 'genre.csv'
+LOAD DATA LOCAL INFILE 'genre.csv'
 INTO TABLE Genre
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES (ID,GenreIsNonGame,GenreIsIndie,GenreIsAction,GenreIsAdventure,GenreIsCasual,GenreIsStrategy,GenreIsRPG,GenreIsSimulation,GenreIsEarlyAccess,GenreIsFreeToPlay,GenreIsSports,GenreIsRacing,GenreIsMassivelyMultiplayer);
 
-LOAD DATA INFILE 'linux.csv'
+LOAD DATA LOCAL INFILE 'linux.csv'
 INTO TABLE RequirementsLinux
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES (ID,LinuxMinReqsText,LinuxRecReqsText);
 
-LOAD DATA INFILE 'mac.csv'
+LOAD DATA LOCAL INFILE 'mac.csv'
 INTO TABLE RequirementsMac
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES (ID,MacMinReqsText,MacRecReqsText);
 
-LOAD DATA INFILE 'pc.csv'
+LOAD DATA LOCAL INFILE 'pc.csv'
 INTO TABLE RequirementsPC
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
